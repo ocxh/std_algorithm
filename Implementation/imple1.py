aa415f07b89e916f90e2a260901a3c6c8b28e4b7
한결같음 <83993373+ocxh@users.noreply.github.com>
2021-08-27 21:15:41 +0900

    implementation 1

---
+++ Implementation/imple1.py
@@ -0,0 +1,23 @@
+n = int(input()) - 1
+mov = list(input().split())
+
+x=0
+y=0
+
+for i in mov:
+    if i == 'L' and y != 0:
+        y -= 1
+    elif i == 'R' and y != n:
+        y += 1
+    elif i == 'U' and x != 0:
+        x -= 1
+    elif i == 'D' and x != n:
+        x += 1
+
+x+=1
+y+=1
+
+
+print(x, y)
+
+
