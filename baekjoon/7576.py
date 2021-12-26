e6465c878b6230298632aaebaa3faff853e8f301
한결같음 <83993373+ocxh@users.noreply.github.com>
2021-12-26 18:18:26 +0900

    2178, 2468, 7576

---
+++ baekjoon/7576.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+m, n = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+count = [[0] * m for _ in range(n)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+q = deque()
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 1:
+            q.append((j, i))
+            visited[i][j] = True
+            count[i][j] = 1
+
+while q:
+    x, y = q.popleft()
+    arr[y][x] = 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < m and 0 <= ny < n:
+            if not visited[ny][nx] and arr[ny][nx] == 0:
+                q.append((nx, ny))
+                visited[ny][nx] = True
+                count[ny][nx] += count[y][x] + 1
+
+danger = False
+for i in range(n):
+    if arr[i].count(0) != 0:
+        danger = True
+
+if danger:
+    print(-1)
+else:
+    mx = 0
+    for i in range(n):
+        if max(count[i]) > mx:
+            mx = max(count[i])
+    print(mx - 1)
