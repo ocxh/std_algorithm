60dd83a5d4dbfd62639de71f797824d6e178eccf
한결같음 <83993373+ocxh@users.noreply.github.com>
2022-01-01 23:49:05 +0900

    그래프 이론: 크루스칼, 위상정렬

---
+++ Graph/Kruskal.py
@@ -0,0 +1,44 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+# 노드의 개수와 간선(union연산)의 개수 입력 받기
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
+
+# 부모 테이블 상에서 부모를 자신으로 초기화
+for i in range(1, v + 1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    # 비용순으로 정렬하기 위해 튜플의 첫 번째 요소를 cost로 설정
+    edges.append((cost, a, b))
+
+# 간선을 비용순으로 정렬
+edges.sort()
+
+# 간선을 하나 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
